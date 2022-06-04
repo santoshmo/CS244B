a771d7d4e7d423c42b0b8e5b5627025927b212cc
thatindiandude <thatindiandude78@gmail.com>
2022-06-03 18:29:39 -0700

    remove unnecessary files

--- class_test.py
+++
@@ -1,8 +0,0 @@
-class TestCacheServer: 
-	def __init__(self, hash_func=hash):
-		self.hash_func = hash_func
-	def getHash(self, obj):
-		return self.hash_func(obj)
-if __name__ == '__main__': 
-	s = TestCacheServer()
-	print(s.getHash('string'))
