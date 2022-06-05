fbe5be0c39878a52cad57ac17b80a9bc28eb1cf6
Nobelle Tay <nobelletay@hotmail.com>
2022-06-04 23:07:59 -0500

    Add large hot object scenario

--- scenarios.py
+++ scenarios.py
@@ -49,3 +49,18 @@ async def BasicWriteAndReadWithNodeFailures(scenario_cost, pcs, num_writes=2, nu
 			num_errors += 1
 	
 	return scenario_cost, num_errors
+
+async def LargeHotObject(scenario_cost, pcs, num_writes=1, num_reads=50):
+	debug("Adding objects")
+	object_keys = get_object_keys("basic", num_writes)
+	insert_cost = await asyncio.gather(*[pcs.Insert(CacheInsertRequest(ObjectToCache(object_key, 1))) for object_key in object_keys])
+	scenario_cost += sum(insert_cost)
+
+	debug("Getting objects")
+	return_value = await asyncio.gather(*[pcs.Get(CacheGetRequest(object_key)) for object_key in object_keys for _ in range(num_reads)])
+	got_object, get_cost = zip(*return_value)
+	got_object = list(got_object)
+	get_cost = list(get_cost)
+	scenario_cost += sum(get_cost)
+	
+	return scenario_cost, 0
